18c4d4ef4affb018e54e526a3db7421a26911cb7
masonnicoletti <mason.f.nicoletti@gmail.com>
2025-11-19 00:37:46 -0500

    WSS connect and kafka producer working

---
+++ coinbase-producer.py
@@ -0,0 +1,76 @@
+import json
+import time
+import logging
+import asyncio
+import websockets
+from quixstreams import Application
+
+kafka_broker = "localhost:19092"
+websocket_uri = "wss://ws-feed.exchange.coinbase.com"
+
+async def stream_coinbase():
+
+    app = Application(
+        broker_address=kafka_broker,
+        loglevel="DEBUG"
+    )
+
+    topic = app.topic(
+        name="crypto-stream",
+        value_serializer="json")
+    
+    try:
+        websocket = await websockets.connect(
+            websocket_uri,
+            ping_interval=20,
+            ping_timeout=20,
+            close_timeout=10
+        )
+        print("Connected to Coinbase Websocket")
+
+        subscribe_payload = {
+        "type": "subscribe",
+        "channels": [
+            {"name": "ticker", "product_ids": ["BTC-USD", "ETH-USD"]},
+            {"name": "level2", "product_ids": ["BTC-USD", "ETH-USD"]}
+            ]
+        }
+
+        await websocket.send(json.dumps(subscribe_payload))
+        
+        while True:
+            try:
+                message = await websocket.recv()
+                data = json.loads(message)
+                print(data)
+
+                key = data['product_id']
+
+                serialized = topic.serialize(key=key, value=data)
+
+                with app.get_producer() as producer:
+                    producer.produce(
+                        topic=topic.name,
+                        key=serialized.key,
+                        value=serialized.value
+                    )
+                    print("Produced a message to Kafka")
+            
+            except websockets.ConnectionClosed as e:
+                print(f"Connection closed: {e}")
+                break
+            
+            except Exception as e:
+                print(f"Error: {e}")
+                continue
+    
+    except Exception as e:
+        print(f"Connection error: {e}")
+        await asyncio.sleep(5)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(stream_coinbase())
+    except KeyboardInterrupt:
+        print("\n Exited out of Streaming")
